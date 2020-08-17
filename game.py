6bb6d7767495d6cfca86eaf31f16632d53d58e5b
Vaibhavnath Jha <Vaibhavnath Jha>
2020-08-17 11:49:14 +0530

    Added response for bad api calls

--- game.py
+++ game.py
@@ -12,25 +12,22 @@ bm = json.loads(open("Bollywood_links.json").read())
 
 def choice(): #1
     print("\t\t\nChoose Category: (1.)Hollywood OR (2.)Bollywood(Default)")
-    if input("\nEnter your Choice: ") == "1": movie_db = hm
-    else: movie_db = bm
+    if input("\nEnter your Choice: ") == "1": category = hm
+    else: category = bm
     os.system('cls')
-    return movie_db
+    return category
 
 def call(db, index):
     headers = {"User-Agent": "Chrome/81.0.4044.122"}
     req = urllib.request.Request(url="http://www.omdbapi.com/?apikey=7a486a08&i=" + urllib.parse.quote(db[index]), headers=headers)
-    keep_trying = True
-    while keep_trying:
-        try:
-            res = urllib.request.urlopen(req)
-        except urllib.error.HTTPError as e:
-            return e.code
-        except urllib.error.URLError as e:
-            return e
-        else:
-            keep_trying = False
-            return json.loads(res.read())
+    try:
+        res = urllib.request.urlopen(req)
+    except urllib.error.HTTPError as e:
+        return e
+    except urllib.error.URLError as e:
+        return e
+    else:
+        return json.loads(res.read())
 
 def alter(title):
     title = title.replace(" ","|")
@@ -51,13 +48,11 @@ def find_all_indexes(input_str, search_str): #2
         d = j + 1
     return l1
 
-
 def digitfinder(title): #3
     regex = '\d+'
     digit = re.findall(regex, title)
     if len(digit) != 0:
         return print('\nTitle contains Number(s)!\n', end='\n')
-
 
 def display(title): #4
     name = []
@@ -75,8 +70,7 @@ def hint(movie, i): #5
             counter = False
             return print("\nshort synopsis/Category of the movie: {}".format(movie['Plot']))
         else:
-            counter = False
-            
+            counter = False      
 
 def info(db): #6
 	if input('\nDo you want Information on the movie? (Y/N):').upper() == "Y":
@@ -91,20 +85,18 @@ def info(db): #6
 	else:
 		return print("\n")
 
-
 def guessed_ltr(ltr, guessedlist, realname): #7
     global max_attempts
     if len(guessedlist) >= 1:
         if (ltr in guessedlist) and (ltr not in realname):
             max_attempts += 1
-            return print('\n*You have already tried this/these letter ' + str(guessedlist))
+            return print('\n')
         elif (ltr in guessedlist):
             return print('\n*You have already tried this/these letter ' + str(guessedlist))
         else:
             return guessedlist.append(ltr)
     else: 
         return guessedlist.append(ltr)
-
 
 exit_token = False
 def fullname_attempt(ltr, title): #8
@@ -114,15 +106,18 @@ def fullname_attempt(ltr, title): #8
             exit_token = True
         else:
             print("You guessed it wrong!")
-            time.sleep(5)
+            time.sleep(3)
             max_attempts = 0
-
 
 max_attempts = 7
 def hangman(): #9
-    movie_db = choice()
-    index = rd.randint(0, 897)
-    movie = call(movie_db, index)
+    category = choice()
+    repeat = True
+    while repeat:
+        index = rd.randint(0, 897)
+        movie = call(category, index)
+        if movie['Response'] != 'False':
+            repeat = False
     title = alter(movie['Title'])
     realname = [char for char in title]
     guessedlist = []
@@ -160,7 +155,7 @@ def hangman(): #9
             for ele in name:
                 print(ele, end=' ')
         if name == realname or exit_token == True:
-            print("\n\nCorrect!\nThe movie was: {}".format(movie['Title']))
+            print("\nCorrect!\n\nThe movie was: {}".format(movie['Title']))
             info(movie)
             break
     return max_attempts*10
